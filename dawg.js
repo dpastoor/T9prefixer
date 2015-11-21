0460bd74fe42f2a5202860012659266e15c4da9c
Albert Tsou <tsou.albert@gmail.com>
2015-11-20 16:15:51 -0800

    basic methods outlined and attributes for trie

--- dawg.js
+++ dawg.js
@@ -1,1 +1,24 @@
 // Directed Acyclic Word Graph for dictionary lookups in T9 advanced datastructures sprint
+
+var DAWG = function(){
+  var newDAWG = Object.create(DAWG.prototype)
+
+  newDAWG.value;
+  newDAWG.children = {};
+  newDAWG.ending = false;
+
+  return newDAWG;
+}
+
+newDAWG.prototype.words = function(node){
+  var words = [];
+  // traverse tree, pushing node.value with node.end == true
+  return words
+}
+newDAWG.prototype.addArray = function(wordArray){
+
+}
+
+newDAWG.prototype.addString = function(wordString){
+
+}
