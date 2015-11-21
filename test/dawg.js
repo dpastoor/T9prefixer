ce7593ad753a61459cbb3ea53783f154ff242f2b
Devin Pastoor <devin.pastoor@gmail.com>
2015-11-21 09:24:16 -0800

    add failing test

--- test/dawg.js
+++ test/dawg.js
@@ -1,8 +1,23 @@
 var test = require('tape');
 var d = require('../src/dawg.js');
 test('dawg works', function (t) {
-    t.plan(1);
+    t.plan(3);
     var dawg = d.DAWG();
+    // can add values
     dawg.addString('h');
     t.equals(dawg.children['h'].value, 'h');
+
+    // can lookup based on a prefix
+    dawg.addString('hello');
+    t.equals(dawg.lookAhead('he'), ['hello']);
+
+    // returns multiple if multiple values available
+    dawg.addString('help');
+    t.equals(dawg.lookAhead('he').sort(), ['hello', 'help'].sort());
+
+
+    /// 'edge' cases
+      // pass empty string
+
+
 });
