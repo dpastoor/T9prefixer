5c4f1a22e4da132f58c84e67acf2fd33deb2676e
Devin Pastoor <devin.pastoor@gmail.com>
2015-11-21 18:08:03 -0800

    gives prefixes when clicking

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import Numpad from './numPad.jsx';
 import Screen from './screen.jsx';
-
+import pre from '../../src/prefixer.js'
 export default class App extends React.Component{
   constructor(props) {
    super(props);
-   this.state = {numbers: ""}
+   this.state = {
+     numbers: "",
+     prefixes: "",
+     words: ""
+   }
  }
  // refs.
  _updateGlobalNum(n) {
-   this.setState({numbers: n})
+   let newNum = this.state.numbers + n;
+   this.setState({numbers: newNum});
+   let prefixes = pre.t9ToPrefixes(parseInt(newNum));
+   this.setState({prefixes: prefixes});
  }
   render() {
     return (
     <div>
       <Screen numbers={this.state.numbers} />
       <Numpad updateGlobalNum={this._updateGlobalNum.bind(this)} />
+      <Screen numbers={this.state.prefixes} />
     </div>
   )
   }
